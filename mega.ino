56add649bb80bf4bfc5242caf0337c4200022339
Andrew Zimazky <zimazky@gmail.com>
2022-11-05 21:23:14 +0300

    Включена функция вывода списка и удаления файлов

--- mega.ino
+++ mega.ino
@@ -63,7 +63,7 @@ hk3022 hydro = hk3022(A2, A3);
 irrigate izone = irrigate(A4);
 ticker tck;
 webserver web;
-const char _version[] = "20220830"; // Версия прошивки 29654 bytes
+const char _version[] = "20221105"; // Версия прошивки 29654 bytes
 
 void setup() {
   Serial.begin(9600);
@@ -297,7 +297,7 @@ void ajax_handler(EthernetClient client, char* req) {
     writeconf();
     return;
   }
-  /*
+  
   /////////////////////////////////////////
   // l:/xxxxx/xxxx Запрос списка файлов в директории /xxxxx/xxxx. Выводим с разделителем ';'
   /////////////////////////////////////////
@@ -350,7 +350,7 @@ void ajax_handler(EthernetClient client, char* req) {
     client.println();
     return;
   }
-*/
+
   client.println(http501);
   client.println();
   
